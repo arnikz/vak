4b624c39f264f7f27223de66bcaf239bc6d3f539
arnikz <a.kuzniar@esciencecenter.nl>
2021-03-07 12:34:15 +0100

    Fix: wait longer before querying XML elem.

--- ping.py
+++ ping.py
@@ -4,24 +4,24 @@ import sys
 
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
 
 def main():
     url = "https://www.old.korona.gov.sk/covid-19-vaccination-form.php"
-    xpath = "/html/body/div[5]/form/div[1]/div[4]/button"
+    expr = "/html/body/div[5]/form/div[1]/div[4]/button"
     exit_code = 0
+    duration = 20
     options = Options()
     options.log.level = "trace"
     options.add_argument("--headless")  
-    driver = webdriver.Firefox(options=options)
-    driver.implicitly_wait(5)
-    driver.get(url)
-    driver.refresh()
-    elem = driver.find_element_by_xpath(xpath)
-    reg,n = elem.text.split("\n")
-    if int(n) != 0:
-        exit_code = 1
-    print("{}: {}".format(reg, n))
-    driver.quit()
+    with webdriver.Firefox(options=options) as driver:
+        driver.get(url)
+        driver.implicitly_wait(duration)
+        elem = driver.find_element_by_xpath(expr)
+        reg,n = elem.text.split("\n")
+        if int(n) != 0:
+            exit_code = 1
+        print("{}: {}".format(reg, n))
     sys.exit(exit_code)
 
 if __name__ == '__main__':
